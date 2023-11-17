c89cb2a08d596458962de4cd9c653996283d2368
Mikhail Seleznyov <Mikhail.Seleznyov@skoltech.ru>
2023-11-17 15:19:39 +0300

    Update evaluation scripts, add utilities for gathering results in a table

---
+++ xcomet/gather_results.py
@@ -0,0 +1,27 @@
+import os
+import pandas as pd
+from utils import load_json
+
+def main():
+    results_dir_name = "results"
+    all_language_pairs = ("en-de", "en-ru", "zh-en")
+
+    paths = [
+        *[f"{results_dir_name}/xcomet_xl/{language_pair}/report.json" for language_pair in all_language_pairs],
+        #*[f"{results_dir_name}/xcomet_xxl/{language_pair}/report.json" for language_pair in all_language_pairs],
+    ]
+
+    records = [load_json(path) for path in paths]
+    results = pd.DataFrame(records)
+
+    interesting_columns = [
+        "model", "lp", "kendall_correlation", "peak_memory_mb", "system_level_score", "prediction_time", "model_load_time", "domain", "year"
+    ]
+
+    results = results[interesting_columns]
+    results.sort_values(by=["model", "lp"])
+    results.to_csv(f"{results_dir_name}/summary.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
