d94378b0a0f17c43d7dd90fa13acdecd01df534f
Seleznyov Mikhail <simplymike8@gmail.com>
2023-11-26 00:34:36 +0300

    Reorganize results folder structure, adapt gather_results script

--- xcomet/gather_results.py
+++ xcomet/gather_results.py
@@ -3,25 +3,36 @@ import pandas as pd
 from utils import load_json
 
 def main():
+    prefix = "with_reference"
+    experiment_names = ["vanilla", "8bit", "4bit"]
+    models = ["xcomet_xl", "xcomet_xxl"]
+    
     results_dir_name = "results"
     all_language_pairs = ("en-de", "en-ru", "zh-en")
     other_language_pairs = ("en-de", "en-es", "en-zh")
-    output_name = "summary.csv"
+    
+    output_name = f"{prefix}_summary.csv"
 
     paths = [
-        *[f"{results_dir_name}/vanilla_xcomet_xl/{language_pair}/report.json" for language_pair in all_language_pairs],
-        *[f"{results_dir_name}/vanilla_xcomet_xxl/{language_pair}/report.json" for language_pair in all_language_pairs],
+        f"{results_dir_name}/{prefix}/{experiment_name}_{model_name}/{language_pair}/report.json" 
+            for language_pair in (other_language_pairs if prefix == "no_reference" else all_language_pairs)
+            for experiment_name in experiment_names
+            for model_name in models
     ]
+    paths = [path for path in paths if os.path.exists(path)]
 
     records = [load_json(path) for path in paths]
     results = pd.DataFrame(records)
+    results["experiment_name"] = [path.split("/")[2].split("_")[0] for path in paths]
+    results["setup"] = prefix
 
     interesting_columns = [
-        "model", "lp", "kendall_correlation", "peak_memory_mb", "system_level_score", "prediction_time", "model_load_time", "domain", "year"
+        "model", "lp", "experiment_name", "kendall_correlation", "peak_memory_mb", \
+        "system_level_score", "prediction_time", "model_load_time", "domain", "year", "setup",
     ]
 
     results = results[interesting_columns]
-    results.sort_values(by=["model", "lp"])
+    results.sort_values(by=["model", "lp", "experiment_name"], inplace=True)
     results.to_csv(f"{results_dir_name}/{output_name}", index=False)
 
 
