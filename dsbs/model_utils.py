58e0835eb46a70d1f5af25afb525e4fcf8a69528
Daniil Larionov <rexhaif.io@gmail.com>
2024-03-27 16:21:50 +0100

    Added new files and scripts for preprocessing and training acceleration. Updated debug configuration and generating explanations.

---
+++ dsbs/model_utils.py
@@ -0,0 +1,210 @@
+# Copyright 2023 The Distilling-step-by-step authors
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import torch
+from typing import Any, Dict, List, Optional, Tuple, Union
+from torch import nn
+from transformers import DataCollatorForSeq2Seq
+from transformers import Seq2SeqTrainer
+from transformers import PreTrainedTokenizer
+
+
+def length_fn(examples):
+    return {'length': [len(x) for x in examples['input_ids']]}
+
+
+def get_tokenize_fn(tokenizer: PreTrainedTokenizer, kind: str = '1way'):
+    
+    def tokenize_function_1way(examples):
+        model_inputs = tokenizer(examples['input'], max_length=1024, truncation=True)
+        
+        labels = tokenizer(text_target=examples['class'], max_length=16, truncation=True)
+    
+        model_inputs['labels'] = labels['input_ids']
+    
+        return model_inputs
+
+    def tokenize_function_2way(examples):
+        model_inputs = tokenizer(['predict: ' + text for text in examples['input']], max_length=1024, truncation=True)
+        expl_model_inputs = tokenizer(['explain: ' + text for text in examples['input']], max_length=1024, truncation=True)
+        model_inputs['expl_input_ids'] = expl_model_inputs['input_ids']
+        model_inputs['expl_attention_mask'] = expl_model_inputs['attention_mask']
+    
+        label_output_encodings = tokenizer(text_target=examples['class'], max_length=16, truncation=True)
+        rationale_output_encodings = tokenizer(text_target=examples['rationale'], max_length=256, truncation=True)
+
+        model_inputs['labels'] = label_output_encodings['input_ids']
+        model_inputs['expl_labels'] = rationale_output_encodings['input_ids']
+    
+        return model_inputs
+
+    def tokenize_function_3way(examples):
+        model_inputs = tokenizer(['predict: ' + text for text in examples['input']], max_length=1024, truncation=True)
+        expl_model_inputs = tokenizer(['explain good: ' + text for text in examples['input']], max_length=1024, truncation=True)
+        antiexpl_model_inputs = tokenizer(['explain bad: ' + text for text in examples['input']], max_length=1024, truncation=True)
+        model_inputs['expl_input_ids'] = expl_model_inputs['input_ids']
+        model_inputs['expl_attention_mask'] = expl_model_inputs['attention_mask']
+        model_inputs['antiexpl_input_ids'] = antiexpl_model_inputs['input_ids']
+        model_inputs['antiexpl_attention_mask'] = antiexpl_model_inputs['attention_mask']
+    
+        label_output_encodings = tokenizer(text_target=examples['class'], max_length=16, truncation=True)
+        rationale_output_encodings = tokenizer(text_target=examples['rationale'], max_length=256, truncation=True)
+        antirationale_output_encodings = tokenizer(text_target=examples['antirationale'], max_length=256, truncation=True)
+    
+        model_inputs['labels'] = label_output_encodings['input_ids']
+        model_inputs['expl_labels'] = rationale_output_encodings['input_ids']
+        model_inputs['antiexpl_labels'] = antirationale_output_encodings['input_ids']
+    
+        return model_inputs
+
+    fns = {
+        '1way': tokenize_function_1way,
+        '2way': tokenize_function_2way,
+        '3way': tokenize_function_3way
+    }
+    assert kind in fns.keys(), f"Kind can only be one of: {fns.keys()}"
+    return fns[kind]
+        
+
+class DataCollator1Way(DataCollatorForSeq2Seq):
+
+    def __call__(self, inputs):
+        pred_features = super().__call__([
+            {
+                'input_ids': item['input_ids'],
+                'attention_mask': item['attention_mask'],
+                'labels': item['labels']
+            } for item in inputs
+        ])
+        return {'pred': pred_features}
+
+class DataCollator2Way(DataCollatorForSeq2Seq):
+
+    def __call__(self, inputs):
+        pred_features = super().__call__([
+            {
+                'input_ids': item['input_ids'],
+                'attention_mask': item['attention_mask'],
+                'labels': item['labels']
+            } for item in inputs
+        ])
+        expl_features = super().__call__([
+            {
+                'input_ids': item['expl_input_ids'],
+                'attention_mask': item['expl_attention_mask'],
+                'labels': item['expl_labels']
+            } for item in inputs
+        ])
+        return {
+            'pred': pred_features,
+            'expl': expl_features
+        }
+
+class DataCollator3Way(DataCollatorForSeq2Seq):
+
+    def __call__(self, inputs):
+        pred_features = super().__call__([
+            {
+                'input_ids': item['input_ids'],
+                'attention_mask': item['attention_mask'],
+                'labels': item['labels']
+            } for item in inputs
+        ])
+        expl_features = super().__call__([
+            {
+                'input_ids': item['expl_input_ids'],
+                'attention_mask': item['expl_attention_mask'],
+                'labels': item['expl_labels']
+            } for item in inputs
+        ])
+        antiexpl_features = super().__call__([
+            {
+                'input_ids': item['antiexpl_input_ids'],
+                'attention_mask': item['antiexpl_attention_mask'],
+                'labels': item['antiexpl_labels']
+            } for item in inputs
+        ])
+        return {
+            'pred': pred_features,
+            'expl': expl_features,
+            'antiexpl': antiexpl_features
+        }
+
+
+class TaskPrefixTrainer(Seq2SeqTrainer):
+    def __init__(
+        self, alpha1: float = 0.0,
+        alpha2: float = 0.0, 
+        kind: str = '1way', 
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.alpha1 = alpha1
+        self.alpha2 = alpha2
+        assert kind in {'1way', '2way', '3way'}
+        self.kind = kind
+
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        if self.kind == '1way':
+            pred_outputs = model(**inputs['pred'])
+            loss = pred_outputs.loss
+            return (
+                loss,
+                {
+                    'pred': pred_outputs
+                }
+            ) if return_outputs else loss
+        elif self.kind == '2way':
+            pred_outputs = model(**inputs['pred'])
+            expl_outputs = model(**inputs['expl'])
+            loss = self.alpha1 * pred_outputs.loss + (1. - self.alpha1) * expl_outputs.loss
+            return (
+                loss,
+                {
+                    'pred': pred_outputs,
+                    'expl': expl_outputs
+                }
+            ) if return_outputs else loss
+        else:
+            # 3way
+            pred_outputs = model(**inputs['pred'])
+            expl_outputs = model(**inputs['expl'])
+            antiexpl_outputs = model(**inputs['antiexpl'])
+            loss = self.alpha1 * pred_outputs.loss + self.alpha2 * expl_outputs.loss + (1 - self.alpha1 - self.alpha2) * antiexpl_outputs
+            return (
+                loss,
+                {
+                    'pred': pred_outputs,
+                    'expl': expl_outputs,
+                    'antiexpl': antiexpl_outputs
+                }
+            ) if return_outputs else loss
+
+    def prediction_step(
+        self,
+        model: nn.Module,
+        inputs: Dict[str, Union[torch.Tensor, Any]],
+        prediction_loss_only: bool,
+        ignore_keys: Optional[List[str]] = None
+    ) -> Tuple[Optional[float], Optional[torch.Tensor], Optional[torch.Tensor]]:
+
+        pred_outputs = super().prediction_step(model, inputs['pred'], prediction_loss_only=False, ignore_keys=ignore_keys)
+        loss = pred_outputs[0]
+        return (
+            loss,
+            pred_outputs[1],
+            pred_outputs[2]
+        )
---
+++ dsbs/train.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+# coding: utf-8
+# srun --pty --partition=gpu_8,gpu_4,gpu_4_a100,gpu_4_h100 --time=1:00:00 --gres=gpu:2 --cpus-per-task=16 --mem=128G accelerate launch train.py -c ./config/debug.yaml 
+
+from rich import print
+import datasets as ds
+import model_utils as mu
+import transformers as tr
+import os
+import time
+import torch
+from omegaconf import OmegaConf
+torch.set_float32_matmul_precision("medium")
+import argparse as ap
+from rich.logging import RichHandler
+import logging
+
+logging.basicConfig(
+    level="INFO",
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[RichHandler(rich_tracebacks=True)]
+)
+
+logger = logging.getLogger(__name__)
+
+logger.info(f"Env vars: {os.environ}")
+
+def main(config_path: str):
+    config = OmegaConf.load(config_path)
+    
+    arguments = tr.Seq2SeqTrainingArguments(
+        output_dir=f"./output/{config.misc.model_name}", 
+        overwrite_output_dir=True,
+        remove_unused_columns=False,
+        do_train=True, do_eval=True,
+        evaluation_strategy='steps', 
+        eval_steps=100,
+        logging_strategy='steps', 
+        logging_steps=100,
+        predict_with_generate=True, 
+        prediction_loss_only=False, 
+        per_device_train_batch_size=config.batch.train_size, 
+        per_device_eval_batch_size=config.batch.eval_size,
+        gradient_accumulation_steps=config.batch.accumulation,
+        learning_rate=config.train.lr, 
+        weight_decay=config.train.weight_decay,
+        max_grad_norm=config.train.max_grad_norm, 
+        max_steps=config.train.max_steps,
+        lr_scheduler_type=config.train.lr_scheduler, 
+        warmup_ratio=config.train.warmup_ratio,
+        save_strategy='steps', 
+        save_steps=100, 
+        save_safetensors=True, 
+        group_by_length=True, length_column_name='length',
+        fp16=True, fp16_opt_level='O2'
+    )
+
+    logging.info(f"Running with arguments: {arguments}")
+
+    logging.info(f"Loading dataset from {config.misc.dataset}")
+    with arguments.main_process_first():
+        t0 = time.perf_counter()
+        dataset = ds.Dataset.from_json(config.misc.dataset)
+        t_elapsed = time.perf_counter() - t0
+        logging.info(f"Dataset Loaded in {t_elapsed:.4f} seconds")
+    
+    if config.misc.subsample:
+        logger.info(f"Subsampling to {config.misc.subsample_size} examples")
+        dataset = dataset.select(range(config.misc.subsample_size))
+
+    logging.info(f"Splitting into train-dev")
+    dataset = dataset.train_test_split(test_size=config.misc.dev_size, seed=config.misc.seed)
+    
+    effective_batch_size = arguments.per_device_train_batch_size * arguments.gradient_accumulation_steps
+    n_epochs = (effective_batch_size * arguments.max_steps * arguments.n_gpu) / len(dataset['train'])
+    logger.info(f"Effective # of epochs: {n_epochs:.4f}")
+
+    logger.info(f"Loading model {config.misc.base_model_name}")
+    t0 = time.perf_counter()
+    model = tr.AutoModelForSeq2SeqLM.from_pretrained(config.misc.base_model_name)
+    tokenizer = tr.AutoTokenizer.from_pretrained(config.misc.base_model_name)
+    t_elapsed = time.perf_counter() - t0
+    logger.info(f"Model loaded in {t_elapsed:.4f} seconds")
+    
+    
+    tokenize_fn = mu.get_tokenize_fn(
+        tokenizer=tokenizer,
+        kind=config.misc.kind
+    )
+
+    with arguments.main_process_first():
+        logger.info("Tokenizing")
+        dataset = dataset.map(
+            tokenize_fn, 
+            batched=True, 
+            batch_size=128, 
+            num_proc=8,
+            remove_columns=dataset['train'].column_names
+        )
+        
+        dataset = dataset.map(
+            mu.length_fn,
+            batched=True,
+            batch_size=128,
+            num_proc=8
+        )
+    
+    
+    data_collator = mu.DataCollator2Way(tokenizer, padding=True, pad_to_multiple_of=8, max_length=1024)
+    
+    import numpy as np
+    def compute_metrics_text(tokenizer):
+        def compute_metrics(eval_pred):
+            predictions, labels = eval_pred
+            decoded_preds = tokenizer.batch_decode(predictions[0], skip_special_tokens=True)
+    
+            labels = np.where(labels[0] != -100, labels[0], tokenizer.pad_token_id)
+            decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+    
+            acc = np.mean(np.array(decoded_preds) == np.array(decoded_labels))
+    
+            return {'accuracy': acc}
+    
+        return compute_metrics
+    
+    trainer = mu.TaskPrefixTrainer(
+        alpha1=config.misc.alpha1,
+        alpha2=config.misc.alpha2,
+        kind=config.misc.kind,
+        data_collator=data_collator,
+        model=model,
+        args=arguments,
+        train_dataset=dataset['train'],
+        eval_dataset=dataset['test'],
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics_text(tokenizer)
+    )
+
+    logger.info("Training")
+    
+    trainer.train()
+    
+    def do_test(idx: int = 0, n: int = 2):
+        examples = dataset['test'][idx:idx+n]
+        result = []
+        for i, ex in enumerate(examples['input_ids']):
+            result.append(f"{i} => {tokenizer.decode(ex)} => [{tokenizer.decode(examples['labels'][i])}]")
+    
+        examples.pop('labels')
+        examples.pop("length")
+        if config.misc.kind != "1way":
+            examples.pop("expl_input_ids")
+            examples.pop("expl_labels")
+            examples.pop("expl_attention_mask")
+            if config.misc.kind == "3way":
+                examples.pop("antiexpl_input_ids")
+                examples.pop("antiexpl_labels")
+                examples.pop("antiexpl_attention_mask")
+        
+        examples = tokenizer.pad(examples, return_attention_mask=True, return_tensors='pt')
+        
+        with torch.inference_mode():
+            examples = {
+                k: v.to(model.device) for k, v in examples.items()
+            }
+            outputs = model.generate(**examples)
+            outputs = outputs.cpu().numpy()
+    
+        for i, out in enumerate(outputs):
+            result[i] += f" *Predicted*: [{tokenizer.decode(out, skip_special_tokens=True)}]"
+    
+        for item in result:
+            print(item)
+    
+    do_test(0, 10)
+
+
+if __name__ == "__main__":
+    parser = ap.ArgumentParser(prog="train.py")
+    parser.add_argument("-c", "--config", type=str, help="Path to yaml config")
+    args = parser.parse_args()
+    main(args.config)
