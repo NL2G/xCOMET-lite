1b55038eef307faaa351c7dd314b92301a4cfd5d
Vasiliy Vyskov <vyskov@nlp2.skoltech.ru>
2024-06-16 01:28:57 +0300

    Extend reduce results script

--- xcomet/reduce_results.py
+++ xcomet/reduce_results.py
@@ -2,25 +2,36 @@ import os
 import argparse
 import json
 import pathlib
+import numpy as np
 
 
 def make_result_dict():
-    return {
+    result = {
         key: 0.0
         for key in (
             'peak_memory_mb_max',
             'peak_memory_mb_mean',
-            'samples_per_second_macro',
             'samples_per_second_micro',
             'prediction_time',
             'kendall_score_weighted',
             'system_score_weighted',
             'kendall_score_mean',
+            'kendall_score_std',
             'system_score_mean',
             'dataset_length',
             'total'
         )
     }
+    result.update(
+        {
+            'samples_per_second_macro_max': 0,
+            'samples_per_second_macro_mean': 0,
+            'samples_per_second_macro_std': 0,
+            'samples_per_second_macro_median': [],
+            'samples_per_second_macro_min': float('+inf')
+        }
+    )
+    return result
 
 
 def make_parser():
@@ -47,23 +58,29 @@ def parse_results(root, model_name):
                 result['peak_memory_mb_mean'] += data['peak_memory_mb']
                 result['prediction_time'] += data['prediction_time']
                 result['samples_per_second_micro'] += data['prediction_time'] * data.get('samples_per_second', 0)
-                result['samples_per_second_macro'] += data.get('samples_per_second', 0)
+                result['samples_per_second_macro_min'] = min(result['samples_per_second_macro_min'], data.get('samples_per_second', 0))
+                result['samples_per_second_macro_mean'] += data.get('samples_per_second', 0)
+                result['samples_per_second_macro_std'] += data.get('samples_per_second', 0) ** 2
+                result['samples_per_second_macro_median'].append(data.get('samples_per_second', 0))
+                result['samples_per_second_macro_max'] = max(result['samples_per_second_macro_max'], data.get('samples_per_second', 0))
                 result['kendall_score_weighted'] += data['dataset_length'] * data['kendall_correlation']
                 result['system_score_weighted'] += data['dataset_length'] * data['system_level_score']
                 result['kendall_score_mean'] += data['kendall_correlation']
+                result['kendall_score_std'] += data['kendall_correlation'] ** 2
                 result['system_score_mean'] += data['system_level_score']
                 result['total'] += 1
         result['kendall_score_weighted'] /= result['dataset_length']
         result['system_score_weighted'] /= result['dataset_length']
         result['samples_per_second_micro'] /= result['prediction_time']
-        result['samples_per_second_macro'] /= result['total']
+        result['samples_per_second_macro_median'] = np.median(result['samples_per_second_macro_median'])
+        result['samples_per_second_macro_mean'] /= result['total']
+        result['samples_per_second_macro_std'] = (result['samples_per_second_macro_std'] / result['total'] - result['samples_per_second_macro_mean'] ** 2) ** 0.5
         result['kendall_score_mean'] /= result['total']
+        result['kendall_score_std'] = (result['kendall_score_std'] / result['total'] - result['kendall_score_mean'] ** 2) ** 0.5
         result['system_score_mean'] /= result['total']
         result['system_score_mean'] /= result['total']
         result['peak_memory_mb_mean'] /= result['total']
         result = {key: round(value, 3) for key, value in result.items()}
-        for key in ('samples_per_second_macro', 'samples_per_second_micro', 'prediction_time'):
-            result[key] = round(result[key])
         report[type_] = result
     return report
 
